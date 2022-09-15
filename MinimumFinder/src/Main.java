d0f91eb272ef78e1979fb7ecacf5fc3b5acff689
Ben Keeley <bkeeley@student.ubc.ca>
2022-09-14 19:21:54 -0700

    Added feature 1. Created a static method that determines the minumum digit of a given int and wrapped it in a MinFinder class. Setup the program to read an int from the user in the console and to output the calculated min digit.

--- MinimumFinder/src/Main.java
+++ MinimumFinder/src/Main.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Type in an integer or a string");
+		
+	    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+
+	    String s = scanner.nextLine();
+	    
+	    while(!s.equals("bye")) {
+	    
+	    try { 
+	    int i = Integer.parseInt(s);
+	    
+	    System.out.println("The smallest digit is: " + MinFinder.findMinimumDigit(i));
+	   
+	    }
+	    catch(NumberFormatException e){
+	    	System.out.println("Error " + e.toString() );
+	    	e.printStackTrace();
+	    }
+	    
+	    s = scanner.nextLine();
+	    
+	    }
 	}
 
 }
